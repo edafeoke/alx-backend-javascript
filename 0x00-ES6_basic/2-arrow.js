234c9f0dc73c24908282606020a0b16d1724f34d
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-05 06:28:49 -0700

    Added 3-default-parameter

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+const getNeighborhoodsList =()=> {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = (newNeighborhood) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+}
+
+export default getNeighborhoodsList;
