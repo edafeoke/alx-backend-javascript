dddb24c0cc33f8bd880514f3cccc9700b8d32f4d
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-06 15:55:32 -0700

    Added 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-  })
-  .then(() => {
-    return {
-	    status : 200,
-	    body : 'success'
-    };
-  })
-  .catch(err => err);
+  }, (err) => err);
 }
