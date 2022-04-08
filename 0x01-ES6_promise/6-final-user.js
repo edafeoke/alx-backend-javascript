0eb80b74a3e8d0642effb1e6854bb54ef199c598
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-08 12:17:44 -0700

    Added task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject.js'
 export default function handleProfileSignup(firstname, lastname, filename) {
   return Promise.all([
     signUpUser(firstname, lastname),
-    uploadPhoto(filename)
+    uploadPhoto(filename),
   ])
   .then(values => values)
   .catch(err => err);
