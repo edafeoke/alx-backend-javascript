6fc948dd5343b12ff652c7a905106fdc96fb3eb4
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-08 11:43:46 -0700

    Added task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
+    console.log('Got a response from the API'));
     return {status: 200, body: 'success'};
   })
-  .then(()=> console.log('Got a response from the API'));
   .catch((err)=> {
     return err;
   });
