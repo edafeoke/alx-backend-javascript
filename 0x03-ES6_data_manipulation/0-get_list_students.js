a3369c2b75c9616a56699e6bde708c4bfb745444
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-11 01:44:20 -0700

    Added 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents () {
+  return [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    { id: 2,
+      firstname: 'James',
+      location: 'San Francisco',
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco',
+    }
+  ];
+}
