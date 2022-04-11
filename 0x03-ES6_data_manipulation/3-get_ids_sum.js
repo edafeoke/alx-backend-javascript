beaaf70367f4e990d5cbcca919e42cafcb4e40a8
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-11 13:46:37 -0700

    Added task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,4 @@
 export default function getStudentIdsSum (arr) {
-  if (!Array.isArray(arr)) return [];
   const ids = arr.map(item => item.id);
   return ids.reduce((total, currentValue) => total + currentValue, 0);
 }
