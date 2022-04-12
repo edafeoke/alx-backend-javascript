e789caf14b2a120956238be6579e7dfa9cf502af
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-12 05:28:47 -0700

    Added 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,5 @@
 export default function getStudentIdsSum (arr) {
-  return arr.reduce((total.id, currentValue.id) => total.id + currentValue.id, 0);
-}
+  return arr.map((e) => e.id)
+	.reduce((total, currentValue) => {
+	  return total + currentValue;
+	}, 0);
